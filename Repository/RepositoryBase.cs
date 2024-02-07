6f763841112bfe15102279da024fae1c55baa9ee
unknown <pedro1029vega@gmail.com>
2024-02-06 22:13:47 -0600

    actualizacion de diagrama

--- Repository/RepositoryBase.cs
+++ Repository/RepositoryBase.cs
@@ -12,6 +12,7 @@ public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
         {
             AppDbContext = repositoryContext; 
         }
+        
         public IQueryable<T> FindAll() => AppDbContext.Set<T>().AsNoTracking();
         public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression) => 
             AppDbContext.Set<T>().Where(expression).AsNoTracking();
